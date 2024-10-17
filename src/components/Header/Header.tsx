ea5dc1aead1e9e9ea14545b3be4af4c10c750126
Thesk1fmeister <thestudio.enjoy@gmail.com>
2024-10-17 16:07:20 +0300

    edited: changes in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,43 @@
-import Image from 'next/image'
-import React from 'react'
-import Navigation from '../Navigation/Navigation'
+import { useState, useEffect } from 'react'
 import { usePathname } from 'next/navigation'
+import Image from 'next/image'
+import Navigation from '../Navigation/Navigation'
 
 const Header = () => {
   const pathname = usePathname()
-  console.log(pathname, 'pathname')
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
 
   if (pathname !== '/booking/shop-custom') {
     return (
-      <div className='bg-primary-black flex flex-col gap-[3.5rem] pt-[4.375rem] items-center justify-center w-full h-[205px]'>
-        <div>
-          <Image src={'/images/site-logo.png'} alt='logo' width={220} height={50} />
+      <div className={`sticky top-0 z-50 transition-all duration-300 ${isScrolled ? 'bg-white' : 'bg-primary-black'}`}>
+        <div
+          className={`flex flex-col items-center  w-full  ${
+            isScrolled
+              ? 'gap-[1.75rem] h-[186px] pt-[2.188rem] justify-between'
+              : ' justify-center gap-[3.5rem] h-[205px] pt-[4.375rem]'
+          } transition-all duration-300`}
+        >
+          <div>
+            <Image
+              src={isScrolled ? '/images/logo-black-text.webp' : '/images/site-logo.png'}
+              alt='logo'
+              width={250}
+              height={40}
+            />
+          </div>
+          <Navigation isScrolled={isScrolled} />
         </div>
-        <Navigation />
       </div>
     )
   }
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { navigationLinks } from '@/constants/navigation'
 import Link from 'next/link'
+import { cn } from '@/lib/utils'
 
-const Navigation = () => {
+type TNavigationProps = {
+  isScrolled: boolean
+}
+const Navigation = ({ isScrolled }: TNavigationProps) => {
   return (
     <div className='flex text-secondary-white pb-[1.375rem] flex-col justify-between'>
       <ul className='flex gap-9'>
         {navigationLinks.map(item => (
-          <li key={item.name} className='uppercase font-black'>
-            <Link className='text-xs' href={item.link}>
+          <li key={item.name} className={cn('uppercase text-white', { '!text-primary-black': isScrolled })}>
+            <Link className='text-sm font-bold hover:text-primary-hover-red' href={item.link}>
               {item.name}
             </Link>
           </li>
