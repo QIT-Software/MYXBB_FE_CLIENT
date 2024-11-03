95591c1b9c12b7502914ef295a5c66b5355df2be
Thesk1fmeister <thestudio.enjoy@gmail.com>
2024-11-03 22:52:45 +0200

    edited: MX-669

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -72,48 +72,59 @@ const Header = () => {
   }, [scrollPosition, pathname])
 
   return (
-    <div className={`sticky top-0 z-50 transition-all duration-300 ${isScrolled ? 'bg-white' : 'bg-primary-black'}`}>
-      <div className='w-full flex justify-center'>
-        <div
-          className={`max-w-[1200px] w-full py-2 text-white justify-end text-sm font-medium items-center ${
-            isScrolled ? 'hidden' : 'flex'
-          }`}
-        >
-          <div className='relative' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-            <div className='flex items-center cursor-pointer gap-[5px]'>
-              <MyxIcon name='shop' className='size-4' />
-              <div className='flex items-center gap-[2px] text-sm'>
-                {`$${totalAmount.toFixed(2)}`}
-                {cartItems.length > 0 && (
-                  <span className='ml-[5px] relative bg-red-500 text-white text-[8px] font-bold rounded h-[14px] w-[18px] flex items-center justify-center'>
-                    {cartItems.length}
-                    <span className='rotate-180 absolute top-1/2 left-[-4px] transform -translate-y-1/2 w-0 h-0 border-l-[4px] border-l-red-500 border-y-[4px] border-y-transparent' />
-                  </span>
-                )}
-              </div>
-            </div>
-
-            {isDropdownVisible && (
-              <DropdownCart
-                cartItems={cartItems}
-                totalAmount={totalAmount}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                removeItem={removeItem}
-              />
-            )}
-          </div>
-        </div>
-      </div>
+    <div className='min-h-[250px]'>
       <div
-        className={`flex flex-col items-center w-full transition-all duration-300 ${
-          isScrolled ? 'smooth-transition justify-between' : 'reverse-smooth-transition justify-center'
+        className={`fixed w-full top-0 z-50 transition-all duration-300 ${
+          isScrolled ? 'top-[-36px] bg-white' : 'bg-primary-black'
         }`}
       >
-        <div>
-          <Image src={isScrolled ? '/images/logo-black-text.webp' : '/images/site-logo.png'} alt='logo' width={250} height={40} />
+        <div className='w-full flex justify-center'>
+          <div
+            className={`max-w-[1200px] flex w-full py-2 text-white  translate-y-0 transition-all duration-800 justify-end text-sm font-medium items-center ${
+              isScrolled && 'translate-y-[-100%]'
+            }`}
+          >
+            <div className='relative' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+              <div className='flex items-center cursor-pointer gap-[5px]'>
+                <MyxIcon name='shop' className='size-4' />
+                <div className='flex items-center gap-[2px] text-sm'>
+                  {`$${totalAmount.toFixed(2)}`}
+                  {cartItems.length > 0 && (
+                    <span className='ml-[5px] relative bg-red-500 text-white text-[8px] font-bold rounded h-[14px] w-[18px] flex items-center justify-center'>
+                      {cartItems.length}
+                      <span className='rotate-180 absolute top-1/2 left-[-4px] transform -translate-y-1/2 w-0 h-0 border-l-[4px] border-l-red-500 border-y-[4px] border-y-transparent' />
+                    </span>
+                  )}
+                </div>
+              </div>
+
+              {isDropdownVisible && (
+                <DropdownCart
+                  cartItems={cartItems}
+                  totalAmount={totalAmount}
+                  onMouseEnter={handleMouseEnter}
+                  onMouseLeave={handleMouseLeave}
+                  removeItem={removeItem}
+                />
+              )}
+            </div>
+          </div>
         </div>
-        <Navigation isScrolled={isScrolled} />
+        <div
+          className={`flex flex-col items-center w-full transition-all duration-800 ${
+            isScrolled ? 'smooth-transition justify-between' : 'reverse-smooth-transition justify-center'
+          }`}
+        >
+          <div>
+            <Image
+              src={isScrolled ? '/images/logo-black-text.webp' : '/images/site-logo.png'}
+              alt='logo'
+              width={250}
+              height={40}
+            />
+          </div>
+          <Navigation isScrolled={isScrolled} />
+        </div>
       </div>
     </div>
   )
