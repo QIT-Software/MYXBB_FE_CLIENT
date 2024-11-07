ce85908ade7d8a1c4ca19212271382a44cb0eb77
Thesk1fmeister <thestudio.enjoy@gmail.com>
2024-11-07 07:25:04 +0200

    MX-671

--- src/app/profile/addresses/page.tsx
+++ src/app/profile/addresses/page.tsx
@@ -1,7 +1,8 @@
+//@ts-nocheck
 'use client'
 import { Controller, useForm } from 'react-hook-form'
 // import { Input, Button } from '@/components/ui'
-import { use, useState } from 'react'
+import { use, useEffect, useState } from 'react'
 import { Input } from '@/components/ui/Input/Input'
 import { Button } from '@/components/ui/Button/Button'
 import PageHeader from '@/components/PageHeader/PageHeader'
@@ -58,7 +59,7 @@ const AddressBookPage = () => {
     setValue,
     trigger,
     formState: { errors },
-  } = useForm({
+  } = useForm<any>({
     defaultValues: {
       is_shipping_address_equals_billing: false,
       billing_address: profile?.billing_address || {
@@ -81,9 +82,13 @@ const AddressBookPage = () => {
   })
 
   const shipping = watch('shipping_address')
-
-  console.log('shipping', shipping)
   const billing = watch('billing_address')
+
+  useEffect(() => {
+    if (profile) {
+      setValue('billing_address', profile?.billing_address)
+    }
+  }, [profile])
 
   const onSubmit = async (data: any) => {
     let combinedData = {
@@ -141,13 +146,6 @@ const AddressBookPage = () => {
     },
   ]
 
-  const renderAddress = (address: any) => (
-    <div className='text-primary-black'>
-      <div>{`${profile?.first_name} ${profile?.last_name}`}</div>
-      <div>{`${address.full_address}`}</div>
-      <div>{`${profile?.phone}`}</div>
-    </div>
-  )
   const billingRegion = watch('billing_address.region')
   const billingStateOptions = getStateOptions(billingRegion, states)
 
@@ -164,27 +162,33 @@ const AddressBookPage = () => {
         </div>
       ) : (
         <>
-          <div className='flex flex-col gap-5'>
-            <h2 className='text-lg uppercase'>Billing Address</h2>
-            {!profile?.billing_address && !showBillingForm && (
+          {!profile?.billing_address?.full_address && !showBillingForm ? (
+            <div className='flex flex-col gap-5'>
+              <h2 className='text-lg uppercase'>Billing Address</h2>
               <div className='cursor-pointer font-semibold text-base capitalize' onClick={() => setShowBillingForm(true)}>
                 + Add Billing Address
               </div>
-            )}
-          </div>
-
-          {billing.address && !showBillingForm && (
-            <div className='flex justify-between items-center'>
-              {renderAddress(profile.billing_address)}
-              <div className='flex gap-2'>
-                <button onClick={() => handleEdit('billing')}>
-                  <MyxIcon name='edit' width={20} height={20} className='hover:text-primary-red' />
-                </button>
-                <button onClick={() => handleDelete({ billing_address: {} })}>
-                  <MyxIcon name='delete' width={20} height={20} className='hover:text-primary-red' />
-                </button>
-              </div>
             </div>
+          ) : (
+            <>
+              {!showBillingForm && (
+                <div className='flex justify-between items-center'>
+                  <div className='text-primary-black'>
+                    <div>{`${profile?.first_name} ${profile?.last_name}`}</div>
+                    <div>{`${profile?.billing_address?.full_address}`}</div>
+                    <div>{`${profile?.phone}`}</div>
+                  </div>
+                  <div className='flex gap-2'>
+                    <button onClick={() => handleEdit('billing')}>
+                      <MyxIcon name='edit' width={20} height={20} className='hover:text-primary-red' />
+                    </button>
+                    <button onClick={() => handleDelete({ billing_address: {} })}>
+                      <MyxIcon name='delete' width={20} height={20} className='hover:text-primary-red' />
+                    </button>
+                  </div>
+                </div>
+              )}
+            </>
           )}
 
           {showBillingForm && (
@@ -196,7 +200,7 @@ const AddressBookPage = () => {
                   {...register('billing_address.address', { required: 'Address is required' })}
                 />
                 {errors?.billing_address?.address && (
-                  <span className='text-red-500'>{errors.billing_address.address.message}</span>
+                  <span className='text-red-500'>{errors?.billing_address?.address.message}</span>
                 )}
               </div>
               <div>
@@ -287,144 +291,146 @@ const AddressBookPage = () => {
 
           <div className='border-b border-secondary-light-grey' />
 
-          <div>
+          {!profile?.shipping_address?.full_address && !showShippingForm ? (
             <div className='flex flex-col gap-5'>
               <h2 className='text-lg uppercase'>Shipping Address</h2>
-              {!shipping.address && !showShippingForm && (
-                <div className='cursor-pointer font-semibold text-base capitalize' onClick={() => setShowShippingForm(true)}>
-                  + Add Shipping Address
+              <div className='cursor-pointer font-semibold text-base capitalize' onClick={() => setShowShippingForm(true)}>
+                + Add Shipping Address
+              </div>
+            </div>
+          ) : (
+            <>
+              {!showShippingForm && (
+                <div className='flex justify-between items-center'>
+                  <div className='text-primary-black'>
+                    <div>{`${profile?.first_name} ${profile?.last_name}`}</div>
+                    <div>{`${profile?.shipping_address?.full_address}`}</div>
+                    <div>{`${profile?.phone}`}</div>
+                  </div>
+                  <div className='flex gap-2'>
+                    <button onClick={() => handleEdit('shipping')}>
+                      <MyxIcon name='edit' width={20} height={20} className='hover:text-primary-red' />
+                    </button>
+                    <button onClick={() => handleDelete({ shipping_address: {} })}>
+                      <MyxIcon name='delete' width={20} height={20} className='hover:text-primary-red' />
+                    </button>
+                  </div>
                 </div>
               )}
-            </div>
+            </>
+          )}
 
-            {shipping.address && !showShippingForm && (
-              <div className='flex justify-between items-center'>
-                {renderAddress(profile.shipping_address)}
-                <div className='flex gap-2'>
-                  <button onClick={() => handleEdit('shipping')}>
-                    <MyxIcon name='edit' width={20} height={20} className='hover:text-primary-red' />
-                  </button>
-                  <button onClick={() => handleDelete({ shipping_address: {} })}>
-                    <MyxIcon name='delete' width={20} height={20} className='hover:text-primary-red' />
-                  </button>
-                </div>
-              </div>
-            )}
-
-            {showShippingForm && (
-              <form onSubmit={handleSubmit(onSubmit)} className='grid grid-cols-2 gap-6 mt-4'>
-                <div>
-                  <Label required text='Address' />
-                  <Input
-                    placeholder='Street address'
-                    {...register('shipping_address.address', { required: 'Address is required' })}
-                  />
-                  {errors?.shipping_address?.address && (
-                    <span className='text-red-500'>{errors.shipping_address.address.message}</span>
-                  )}
-                </div>
-                <div className='flex flex-col gap-1'>
-                  <Label text='Region' required />
-                  <Controller
-                    name='shipping_address.region'
-                    control={control}
-                    render={({ field }) => (
-                      <Select
-                        {...field}
-                        styles={customStyles}
-                        value={regionOptions.find((option: TOption) => option.value === field.value)}
-                        options={regionOptions}
-                        placeholder='Region'
-                        onChange={option => {
-                          field.onChange((option as { value: string; label: string })?.value)
-                          setValue('shipping_address.state', '')
-                        }}
-                      />
-                    )}
-                  />
-                  {errors?.shipping_address?.region && (
-                    <span className='text-red-500'>{errors.shipping_address.region.message}</span>
-                  )}
-                </div>
-                <div>
-                  <Label text='Apartment' required />
-                  <Input
-                    placeholder='Aprt, Suite, Bldg.'
-                    {...register('shipping_address.apartment', { required: 'Apartment is required' })}
-                  />
-                  {errors?.shipping_address?.apartment && (
-                    <span className='text-red-500'>{errors.shipping_address.apartment.message}</span>
-                  )}
-                </div>
-                <div>
-                  <Label text='State' required />
-                  <Controller
-                    name='shipping_address.state'
-                    control={control}
-                    render={({ field }) => (
-                      <Select
-                        {...field}
-                        styles={customStyles}
-                        value={shippingStateOptions.find((option: TOption) => option.value === field.value) || null}
-                        options={shippingStateOptions}
-                        placeholder='State'
-                        onChange={option => field.onChange((option as { value: string | number; label: string })?.value)}
-                      />
-                    )}
-                  />
-                  {errors?.shipping_address?.state && (
-                    <span className='text-red-500'>{errors.shipping_address.state.message}</span>
-                  )}
-                </div>
-                <div>
-                  <Label text='City' required />
-                  <Input placeholder='City' {...register('shipping_address.city', { required: 'City is required' })} />
-                  {errors?.shipping_address?.city && <span className='text-red-500'>{errors.shipping_address.city.message}</span>}
-                </div>
-                <div>
-                  <Label text='Zip code' required />
-                  <Input
-                    placeholder='Zip code'
-                    {...register('shipping_address.zip_code', {
-                      pattern: {
-                        value: /^[0-9]+(-[0-9]+)?$/,
-                        message: 'Zip code must be a number or in the format 12345-6789',
-                      },
-                      minLength: {
-                        value: 4,
-                        message: 'Zip code must be at least 4 characters long',
-                      },
-                      maxLength: {
-                        value: 10,
-                        message: 'Zip code must be no longer than 10 characters',
-                      },
-                    })}
-                  />
-                  {errors?.shipping_address?.zip_code && (
-                    <span className='text-red-500'>{errors.shipping_address.zip_code.message}</span>
-                  )}
-                </div>
-                <div className='w-full flex gap-2 items-center'>
-                  <Controller
-                    name='is_shipping_address_equals_billing'
-                    control={control}
-                    render={({ field }) => (
-                      <Checkbox id='is_shipping_address_equals_billing' checked={field.value} onCheckedChange={field.onChange} />
-                    )}
-                  />
-                  <span className={cn('text-secondary-black')}>Shipping Address the same as billing</span>
-                </div>
-                {!isShippingEqualsBilling && (
-                  <div className='col-span-2 flex gap-4'>
-                    <Button type='submit'>Save</Button>
-                    <Button type='button' variant='blackUnderline' onClick={() => setShowShippingForm(false)}>
-                      Cancel
-                    </Button>
-                  </div>
+          {showShippingForm && (
+            <form onSubmit={handleSubmit(onSubmit)} className='grid grid-cols-2 gap-6 mt-4'>
+              <div>
+                <Label required text='Address' />
+                <Input
+                  placeholder='Street address'
+                  {...register('shipping_address.address', { required: 'Address is required' })}
+                />
+                {errors?.shipping_address?.address && (
+                  <span className='text-red-500'>{errors.shipping_address.address.message}</span>
                 )}
-              </form>
-            )}
-          </div>
+              </div>
+              <div className='flex flex-col gap-1'>
+                <Label text='Region' required />
+                <Controller
+                  name='shipping_address.region'
+                  control={control}
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      styles={customStyles}
+                      value={regionOptions.find((option: TOption) => option.value === field.value)}
+                      options={regionOptions}
+                      placeholder='Region'
+                      onChange={option => {
+                        field.onChange((option as { value: string; label: string })?.value)
+                        setValue('shipping_address.state', '')
+                      }}
+                    />
+                  )}
+                />
+                {errors?.shipping_address?.region && (
+                  <span className='text-red-500'>{errors.shipping_address.region.message}</span>
+                )}
+              </div>
+              <div>
+                <Label text='Apartment' required />
+                <Input
+                  placeholder='Aprt, Suite, Bldg.'
+                  {...register('shipping_address.apartment', { required: 'Apartment is required' })}
+                />
+                {errors?.shipping_address?.apartment && (
+                  <span className='text-red-500'>{errors.shipping_address.apartment.message}</span>
+                )}
+              </div>
+              <div>
+                <Label text='State' required />
+                <Controller
+                  name='shipping_address.state'
+                  control={control}
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      styles={customStyles}
+                      value={shippingStateOptions.find((option: TOption) => option.value === field.value) || null}
+                      options={shippingStateOptions}
+                      placeholder='State'
+                      onChange={option => field.onChange((option as { value: string | number; label: string })?.value)}
+                    />
+                  )}
+                />
+                {errors?.shipping_address?.state && <span className='text-red-500'>{errors.shipping_address.state.message}</span>}
+              </div>
+              <div>
+                <Label text='City' required />
+                <Input placeholder='City' {...register('shipping_address.city', { required: 'City is required' })} />
+                {errors?.shipping_address?.city && <span className='text-red-500'>{errors.shipping_address.city.message}</span>}
+              </div>
+              <div>
+                <Label text='Zip code' required />
+                <Input
+                  placeholder='Zip code'
+                  {...register('shipping_address.zip_code', {
+                    pattern: {
+                      value: /^[0-9]+(-[0-9]+)?$/,
+                      message: 'Zip code must be a number or in the format 12345-6789',
+                    },
+                    minLength: {
+                      value: 4,
+                      message: 'Zip code must be at least 4 characters long',
+                    },
+                    maxLength: {
+                      value: 10,
+                      message: 'Zip code must be no longer than 10 characters',
+                    },
+                  })}
+                />
+                {errors?.shipping_address?.zip_code && (
+                  <span className='text-red-500'>{errors.shipping_address.zip_code.message}</span>
+                )}
+              </div>
+              <div className='w-full flex gap-2 items-center'>
+                <Controller
+                  name='is_shipping_address_equals_billing'
+                  control={control}
+                  render={({ field }) => (
+                    <Checkbox id='is_shipping_address_equals_billing' checked={field.value} onCheckedChange={field.onChange} />
+                  )}
+                />
+                <span className={cn('text-secondary-black')}>Shipping Address the same as billing</span>
+              </div>
+              {!isShippingEqualsBilling && (
+                <div className='col-span-2 flex gap-4'>
+                  <Button type='submit'>Save</Button>
+                  <Button type='button' variant='blackUnderline' onClick={() => setShowShippingForm(false)}>
+                    Cancel
+                  </Button>
+                </div>
+              )}
+            </form>
+          )}
         </>
       )}
     </div>
