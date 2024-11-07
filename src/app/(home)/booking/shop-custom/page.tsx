e213f11a8e9309ad036d1a84a493a2c421850cdd
Thesk1fmeister <thestudio.enjoy@gmail.com>
2024-11-07 15:08:23 +0200

    features

--- src/app/(home)/booking/shop-custom/page.tsx
+++ src/app/(home)/booking/shop-custom/page.tsx
@@ -11,9 +11,10 @@ import LipsScrubBlock from '../../components/LipsScrubBlock/LipsScrubBlock'
 import BundlesBlock from '../../components/BundlesBlock/BundlesBlock'
 import BagsBlock from '../../components/BagsBlock/BagsBlock'
 import GIftCardBlock from '../../components/GIftCardBlock/GIftCardBlock'
+import ClipLoader from 'react-spinners/ClipLoader'
 
 const CustomShopPage = () => {
-  const { data: products } = useGetProductsQuery({ is_for_shop: true })
+  const { data: products, isLoading } = useGetProductsQuery({ is_for_shop: true })
   console.log(products, 'products')
   return (
     <div className='flex flex-col py-[4.375rem] w-full'>
@@ -25,24 +26,29 @@ const CustomShopPage = () => {
           </div>
 
           {/* primary item */}
-          <div className='flex justify-center items-center max-w-[1200px] w-full gap-7'>
-            <div className='flex w-1/2'>
-              <Image src={'/images/main-product.webp'} alt='custom shop' width={585} height={585} />
+          {isLoading ? (
+            <div>
+              <ClipLoader color={'red'} loading={true} size={50} />
             </div>
-            <div className='w-1/2 flex flex-col gap-[2.188rem] text-center'>
-              <div className='flex flex-col gap-2.5'>
-                <h1 className='suave-text text-[3.125rem] text-center text-primary-black font-bold'>BALMSHELL LIP BALM</h1>
-                <p className='text-[1.25rem] font-bold text-primary-black'>Our Miracle Lip Perfecter.</p>
-                <p className='text-2xl font-bold text-primary-gray'>
-                  A blend of peptides naturally increase collagen production, creating a naturally plumper lip, improving moisture
-                  and smoothing fine lines.
-                </p>
+          ) : (
+            <div className='flex justify-center items-center max-w-[1200px] w-full gap-7'>
+              <div className='flex w-1/2'>
+                <Image src={products.featured_product.avatar} alt='custom shop' width={585} height={585} />
               </div>
-              <div className='flex items-center justify-center'>
-                <ShopButton link='/booking/shop/0cf14051-82a4-4001-b3e1-70df85a904dc' />
+              <div className='w-1/2 flex flex-col gap-[2.188rem] text-center'>
+                <div className='flex flex-col gap-2.5'>
+                  <h1 className='suave-text text-[3.125rem] text-center text-primary-black font-bold'>
+                    {products.featured_product.name}
+                  </h1>
+                  <p className='text-[1.25rem] font-bold text-primary-black'>Our Miracle Lip Perfecter.</p>
+                  <p className='text-2xl font-bold text-primary-gray'>{products.featured_product.description}</p>
+                </div>
+                <div className='flex items-center justify-center'>
+                  <ShopButton link={`/booking/shop/${products.featured_product.id}`} />
+                </div>
               </div>
             </div>
-          </div>
+          )}
           <div className='flex flex-col gap-[7.5rem] max-w-[1200px] w-full pt-[7.5rem]'>
             <LipstickBlock />
             <LipGlossBlock />
